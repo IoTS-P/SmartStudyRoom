cbe99b5c07d7652b64f26960e9bd6e8b6785f815
Ruibo Lu <reaperlu@hust.edu.cn>
2024-09-20 18:50:14 +0800

    add example motion-sensor
    Signed-off-by: Ruibo Lu <reaperlu@hust.edu.cn>

--- project/CoAP_pir_server/build/cc26x0-cc13x0/launchpad/cc2650/obj/.deps/syscalls.d
+++
@@ -1,5 +0,0 @@
-build/cc26x0-cc13x0//launchpad/cc2650/obj/syscalls.o: \
- ../../../../os/lib/newlib/syscalls.c \
- ../../../../os/net/app-layer/coap/module-macros.h
-
-../../../../os/net/app-layer/coap/module-macros.h:
